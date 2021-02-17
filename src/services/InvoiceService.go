3407b969ee4a9ff3f09ecb41284f973b62b7e3c1
Mihael <mihaelmacuka2@gmail.com>
2021-02-17 19:29:13 +0100

    Encoding error

--- src/services/InvoiceService.go
+++ src/services/InvoiceService.go
@@ -25,17 +25,36 @@ func CreateInvoice(invoiceDto dto.InvoiceDto) (models.Invoice, error) {
 	return invoice, nil
 }
 
+func getLocations(width float64) []float64 {
+	items := make([]float64, 0)
+	items = append(items, width*0.05)
+	items = append(items, width*0.10)
+	items = append(items, width*0.45)
+	items = append(items, width*0.55)
+	items = append(items, width*0.65)
+	items = append(items, width*0.85)
+	items = append(items, width*0.95)
+	return items
+}
+
 func ExportInvoice() {
 	const (
 		headerName    = "PODUZEĆE ZA GRAĐENJE, PRIJEVOZ, TRGOVINU I USLUGE"
 		headerContact = "Videti 121/f, 52404 Sveti Petar u Šumi\ntel: 052/686 - 440; fax: 052/686 - 550\nmob: 098305698\nžiro-račun: Erste bank 2402006 - 1100072567\ne-mail: macuka@pu.t-com.hr\nIBAN: HR7024020061100072567; SWIFT: ESBCHR22\nMBS: 040140254; POR.BR:1415476;"
 		headerOIB     = "OIB: 00645636377"
 	)
+	tableHeaders := []string{
+		"RB",
+		"ROBA/USLUGA",
+		"JM",
+		"KOL",
+		"CIJENA",
+		"IZNOS",
+	}
 	pdf := gofpdf.New("P", gofpdf.UnitPoint, gofpdf.PageSizeA4, "")
 	pdf.AddPage()
-	pageWidth, _ := pdf.GetPageSize()
-	pdf.SetLeftMargin(0.05 * pageWidth)
-	pdf.SetRightMargin(0.05 * pageWidth)
+	pdf.SetCompression(true)
+	pageWidth, pageHeight := pdf.GetPageSize()
 
 	//HEADER
 	pdf.SetFont("times", "", 12)
@@ -68,8 +87,56 @@ func ExportInvoice() {
 	_, h = pdf.GetFontSize()
 	pdf.MultiCell(0, 30, "RAČUN R1 BROJ:a", gofpdf.BorderNone, gofpdf.AlignLeft, false)
 	pdf.SetFont("arial", "", 10)
-	_, h = pdf.GetFontSize()
+	w, h := pdf.GetFontSize()
 	pdf.MultiCell(0, 1.2*h, "DATUM I VRIJEME:A\nMJESTO:a\nVALUTA PLAĆANJA:1\nNAČUN PLAĆANJA:TRANSAKCIJSKI RAČUN", gofpdf.BorderNone, gofpdf.AlignLeft, false)
+
+	//Table
+	currentY := 350.
+	pdf.Line(0.05*pageWidth, currentY, 0.95*pageWidth, currentY)
+	locations := getLocations(pageWidth)
+	pdf.SetFont("arial", "b", 11)
+	for i := 0; i < len(locations)-1; i++ {
+		middle := locations[i] + (locations[i+1]-locations[i])/2
+		halfLength := int64((len(tableHeaders[i])) / 2)
+		pdf.Text(middle-float64(halfLength)*w, currentY-1, tableHeaders[i])
+	}
+
+	pdf.SetFont("arial", "", 11)
+
+	//First row
+	currentY += h
+	for i := 0; i < len(locations)-1; i++ {
+		pdf.MoveTo(locations[i], currentY)
+		pdf.MultiCell(0, 0, "a", gofpdf.AlignMiddle, gofpdf.AlignMiddle, false)
+	}
+	currentY += h
+	pdf.Line(0.05*pageWidth, currentY, 0.95*pageWidth, currentY)
+
+	for i := 0; i < len(locations); i++ {
+		pdf.Line(locations[i], 350, locations[i], currentY)
+	}
+
+	pdf.Line(0.05*pageWidth, currentY, 0.95*pageWidth, currentY)
+
+	//Payment
+	pdf.MoveTo(0, currentY+30)
+	pdf.MultiCell(0, h, "aaab\naaaaaaab\naaaab", gofpdf.BorderNone, gofpdf.AlignRight, false)
+
+	pdf.SetFont("arial", "b", 11)
+	pdf.MultiCell(0, h+30, "Poziv na broj: xy", gofpdf.BorderNone, gofpdf.AlignLeft, false)
+
+	//Footer
+	_, h = pdf.GetFontSize()
+	pdf.MultiCell(0, h, "Porez na dodanu vrijednost se ne obračunava sukladno čl.75.st.3.Zakona o PDV-u.", gofpdf.BorderNone, gofpdf.AlignLeft, false)
+	pdf.MultiCell(0, h, "Prijenos porezne obveze na investitora – REVERSE CHARGE", gofpdf.BorderNone, gofpdf.AlignLeft, false)
+
+	pdf.SetFont("arial", "", 11)
+	w, h = pdf.GetFontSize()
+	pdf.MultiCell(0, h, "U slučaju neplaćanja u roku zaračunavamo zakonsku zateznu kamatu.", gofpdf.BorderNone, gofpdf.AlignLeft, false)
+
+	pdf.Text(0.07*pageWidth, 0.8*pageHeight, "Hiacinta Macuka")
+	pdf.Text(0.75*pageWidth, 0.8*pageHeight, "Vinko Macuka d.i.g")
+	pdf.Text((pageWidth/2)-float64(len("MP"))/2*w, 0.9*pageHeight, "MP")
 
 	err := pdf.OutputFileAndClose("file.pdf")
 	if err != nil {
